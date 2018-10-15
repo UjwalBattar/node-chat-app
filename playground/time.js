c6b2acb2f4900a881a69273ab1c44854ed441d6d
Ujwal Battar <ujwal.battar@gmail.com>
2018-10-14 18:13:12 -0700

    play around with momentjs

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+// var date = moment();
+// date.add(11, 'years').subtract(12, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+console.log(date.format('h:mm a'));
